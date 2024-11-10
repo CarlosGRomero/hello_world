b50285a58df68d67a0b72f26d6552c60e9530f27
Carlos Romero <carlosl70n@yahoo.com>
2024-11-09 19:35:33 -0600

    deleted old files

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
-const port = process.env.PORT || 80;
-const environment = process.env.NODE_ENV || 'development';
 app.listen(port, () => {
-  console.log(`Server running in ${environment} mode at http://localhost:${port}/`);
+    console.log(`App listening at http://localhost:${port}`);
 });
